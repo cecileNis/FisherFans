d29fc08e1b21554436d0e7461099212e86f632ec
cecileNis <cecile.niess@ynov.com>
2023-11-23 16:13:39 +0100

    changements operations

--- src/Entity/Fish.php
+++ src/Entity/Fish.php
@@ -5,23 +5,54 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\FishRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(denormalizationContext: ['groups' => ['fish:write']], security: 'is_granted("ROLE_USER")'),
+        new Get(
+            uriTemplate: '/fish/{id}',
+            security: 'is_granted("ROLE_USER")',
+            normalizationContext: ['groups' => ['fish:read']],
+            name: 'get_fish'
+        ),
+        new Delete(security: 'is_granted("ROLE_USER")'),
+    ],
+    normalizationContext: ['groups' => ['fish:read']],
+    denormalizationContext: ['groups' => ['fish:create', 'fish:update']],
+)]
+
+
 
 #[ORM\Entity(repositoryClass: FishRepository::class)]
-#[ApiResource]
+#[ORM\Table(name: 'fish')]
 class Fish
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['fish:read'])]
     private ?int $id = null;
 
+    #[Assert\NotBlank(groups: ['fish:create'])]
     #[ORM\Column(length: 255)]
+    #[Groups(['fish:read', 'fish:write'])]
     private ?string $url = null;
 
+    #[Assert\NotBlank(groups: ['fish:create'])]
     #[ORM\Column]
+    #[Groups(['fish:read', 'fish:write'])]
     private ?int $size = null;
 
+    #[Assert\NotBlank(groups: ['fish:create'])]
     #[ORM\Column]
+    #[Groups(['fish:read', 'fish:write'])]
     private ?int $weight = null;
 
     public function getId(): ?int
--- src/Entity/FishingLog.php
+++ src/Entity/FishingLog.php
@@ -6,23 +6,59 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\FishingLogRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Put;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: FishingLogRepository::class)]
-#[ApiResource]
+
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(denormalizationContext: ['groups' => ['fishing_log:write']], security: 'is_granted("ROLE_USER")'),
+        new Get(
+            uriTemplate: '/fishing_log/{id}',
+            security: 'is_granted("ROLE_USER")',
+            normalizationContext: ['groups' => ['fishing_log:read']],
+            name: 'get_fishing_log'
+        ),
+        new Put(
+            uriTemplate: '/fishing_log/{id}',
+            security: 'is_granted("ROLE_USER")',
+            normalizationContext: ['groups' => ['fishing_log:read']],
+            denormalizationContext: ['groups' => ['fishing_log:update']],
+            name: 'put_fishing_log'
+        ),
+        new Delete(security: 'is_granted("ROLE_USER")'),
+    ],
+    normalizationContext: ['groups' => ['fishing_log:read']],
+    denormalizationContext: ['groups' => ['fishing_log:create', 'fishing_log:update']],
+)]
+
+
+#[ORM\Entity(repositoryClass: FishRepository::class)]
+#[ORM\Table(name: 'fishing_log')]
 class FishingLog
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['fishing_log:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['fishing_log:read', 'fishing_log:write', 'fishing_log:update'])]
     private ?string $comment = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['fishing_log:read', 'fishing_log:write'])]
     private ?string $location = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['fishing_log:read', 'fishing_log:write'])]
     private ?\DateTimeInterface $date_of_fishing = null;
 
     #[ORM\Column]
